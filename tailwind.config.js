9b9b3ad94826e739780af105f46fa4d3ca39ea79
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2025-01-10 07:45:44 +0300

    feat(index.html): set correct name and icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@
 module.exports = {
 	content: ['./src/**/*.{html,ts}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'background-primary': 'radial-gradient(circle at 50% 150%, #AE4862 0%, #773344 22%, #2C1A30 60%, #12081D 100%)',
+			},
+			colors: {
+				my: {
+					blue: '#0D3B66',
+					orange: '#F87060',
+					burgundy: '#773344',
+					green: '#0C740A',
+					glass: '#FFFFFF10',
+				},
+				font: {
+					primary: '#D9D9D9',
+					secondary: '#A5A5A5',
+					disabled: '#000000',
+					button: '#FFFFFF',
+					authSubtitle: '#48A9A6',
+				},
+			},
+			animation: {
+				'loading-spin': 'loading-spin 2.5s ease-in-out infinite both',
+			},
+			keyframes: {
+				'loading-spin': {
+					'0%': { transform: 'rotate(45deg)' },
+					'100%': { transform: 'rotate(405deg)' },
+				},
+			},
+		},
 	},
-	plugins: [],
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: [],
+	},
 }
