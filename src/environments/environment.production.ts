4981b74c732faddc7fc75adbb90527cdd99a8237
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2025-02-23 14:08:19 +0300

    feat: setup prod environment variables and build

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,6 @@
+export const environment = {
+	production: true,
+	apiUrl: 'https://api.gametrix.org/api',
+	colyseusUrl: 'ws://api.gametrix.org/api',
+	timeout: 10000,
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
+	production: false,
 	apiUrl: 'http://localhost:4000/api',
-	socketUrl: 'http://localhost:4000',
 	colyseusUrl: 'ws://localhost:4000/api',
 	timeout: 10000,
 }
