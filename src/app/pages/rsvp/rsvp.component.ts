10409ba75819443c104d9ab807c896778a15c9e4
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2025-05-30 06:49:41 +0300

    fix: close the rsvp page automatically after 60 seconds

--- src/app/pages/rsvp/rsvp.component.ts
+++ src/app/pages/rsvp/rsvp.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnDestroy, OnInit, signal } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
-import { finalize, Subscription } from 'rxjs'
+import { finalize, interval, scan, Subscription, take, tap } from 'rxjs'
 
 import { ErrorWhileLoadingComponent } from '../../shared/components/error-while-loading/error-while-loading.component'
 import { LoadingComponent } from '../../shared/components/loading/loading.component'
+import { RSVP_PAGE_CLOSING_COUNTDOWN_SECONDS } from '../../shared/constants/common.constants'
 import { ScheduledGame } from '../../shared/types/scheduled-game.type'
-import { startCountdown } from '../../shared/utils/timer.util'
 import { RsvpService } from './rsvp.service'
 
 @Component({
@@ -48,22 +48,31 @@ export class RsvpComponent implements OnInit, OnDestroy {
 				.rsvp(email, gameId)
 				.pipe(finalize(() => this.loaded.set(true)))
 				.subscribe({
-					next: (scheduledGame) => {
-						this.scheduledGame.set(scheduledGame)
-						startCountdown(new Date(scheduledGame.start_time)).subscribe({
-							next: ({ days, hours, minutes, seconds }) => {
-								this.days.set(days)
-								this.hours.set(hours)
-								this.minutes.set(minutes)
-								this.seconds.set(seconds)
-							},
-						})
-					},
+					// next: (scheduledGame) => {
+					// this.scheduledGame.set(scheduledGame)
+					// startCountdown(new Date(scheduledGame.start_time)).subscribe({
+					// 	next: ({ days, hours, minutes, seconds }) => {
+					// 		this.days.set(days)
+					// 		this.hours.set(hours)
+					// 		this.minutes.set(minutes)
+					// 		this.seconds.set(seconds)
+					// 	},
+					// })
+					// },
 				})
 		} else {
 			this.error.set(true)
 			this.loaded.set(true)
 		}
+
+		interval(1000)
+			.pipe(
+				tap((value) => this.seconds.set(RSVP_PAGE_CLOSING_COUNTDOWN_SECONDS - value)),
+				scan((accumulator) => accumulator - 1, RSVP_PAGE_CLOSING_COUNTDOWN_SECONDS + 1),
+				take(RSVP_PAGE_CLOSING_COUNTDOWN_SECONDS + 1),
+				finalize(() => window.close()),
+			)
+			.subscribe()
 	}
 
 	ngOnDestroy() {
