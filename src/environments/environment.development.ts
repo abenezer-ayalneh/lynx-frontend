76b84f9879442733a76902aa7973fada211b4c3c
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2025-06-30 07:10:48 +0300

    style: update responsiveness of homepage

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: false,
-	apiUrl: 'http://localhost:4000/api',
-	colyseusUrl: 'ws://localhost:4000/api',
+	apiUrl: 'http://192.168.8.123:4000/api',
+	colyseusUrl: 'ws://192.168.8.123:4000/api',
 	timeout: 10000,
-	liveKitServerUrl: 'ws://localhost:7880',
+	liveKitServerUrl: 'ws://192.168.8.123:7880',
 }
