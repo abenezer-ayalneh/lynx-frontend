54263ba4a3fb6517f76e48fabdf09c4c26a2b063
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2025-12-22 13:38:54 +0300

    feat(auth): integrate Google login and update environment URLs

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,6 @@
 export const environment = {
 	production: true,
+	appUrl: 'https://gametrix.org',
 	apiUrl: 'https://api.gametrix.org/api',
 	colyseusUrl: 'wss://api.gametrix.org/api',
 	timeout: 10000,
